cc53481b01e3bad71c271475c0d58a285959dadf
Lauren A <laurenhaileyama@gmail.com>
2022-04-11 17:40:51 -0400

    finish tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -45,10 +45,39 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!updateTag[0]) {
+      res.status(404).json({ message: "No tag with this id"});
+      return;
+    }
+    res.status(200).json(updateTag);
+  } catch (err){ 
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({ 
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deleteTag) {
+      res.status(404).json({message: "No tag found with that id"});
+      return;
+    }
+    res.status(200).json(deleteTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
