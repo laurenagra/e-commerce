53ad8df89876aaa43fc321293b5f416f439694df
Lauren A <laurenhaileyama@gmail.com>
2022-04-11 17:35:46 -0400

    update tag-routes file, 2 routes left to write

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,15 +6,41 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [
+        { model: Product},
+      ],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [
+        { model: Product},
+      ],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (err) { 
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
